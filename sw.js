59b2eef5095aa6e2026e6b080d160b4bfec872b1
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-21 00:11:22 +0900

    Bump SW cache v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v3';
+const CACHE = 'task-v4';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
