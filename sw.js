1d77fb9eaac21d8ba2f901a264cabbfe88c7d3ce
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-21 00:17:15 +0900

    Swipe btn colors: grey edit, red delete

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v5';
+const CACHE = 'task-v6';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
