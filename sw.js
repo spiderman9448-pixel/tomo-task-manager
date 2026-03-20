857c979d06330e6a0a9c28bad9fff082786a31c6
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-20 22:50:59 +0900

    PWA support, calendar fix, grey theme

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'task-v1';
+const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
