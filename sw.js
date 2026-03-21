397ccd21d82f9cbdd9f44d8c769994b241b79e7d
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-21 09:05:04 +0900

    Add Firebase Realtime Database + Google Auth sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v6';
+const CACHE = 'task-v7';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
