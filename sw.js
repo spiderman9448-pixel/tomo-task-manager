bd4ebe94cb4464abf9364e6d01d44d02e87e98d3
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-21 09:54:22 +0900

    sw.js: bump cache to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v7';
+const CACHE = 'task-v8';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
