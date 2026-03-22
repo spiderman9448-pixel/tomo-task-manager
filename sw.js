f4c4bcd7728ee8fc1ad4954556bb36f92cce714e
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-22 22:37:06 +0900

    Add calendar connect button in settings for mobile (v15)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v14';
+const CACHE = 'task-v15';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
