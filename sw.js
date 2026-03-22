4523e6b70e31375fd0a3a5d89f7103aad7d980e8
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-22 19:10:35 +0900

    Default categories: only プライベート and 仕事 (v10)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v9';
+const CACHE = 'task-v10';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
