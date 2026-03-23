995949484eebafe386b7e2e4dec9246bd129986b
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-23 10:27:13 +0900

    Linkify memo URLs, tap link opens URL, tap elsewhere edits (v35)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v34';
+const CACHE = 'task-v35';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
