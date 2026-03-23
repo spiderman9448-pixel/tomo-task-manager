9dbb6e5ad125b6ed7ae50a28b775083626ec0e84
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-23 10:44:52 +0900

    Fix: fetch events immediately after OAuth, remove debug alerts (v38)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v37';
+const CACHE = 'task-v38';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
