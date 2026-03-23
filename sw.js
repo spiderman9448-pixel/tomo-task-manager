b027022d963b8b3a3ebe047f2b49f679dcaa5ef0
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-23 09:49:44 +0900

    Fix list swipe z-index, add hover actions for PC (v25)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v24';
+const CACHE = 'task-v25';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
