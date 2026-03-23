46dbb8232d89fb2e47c68a8bd3a34501cb705db0
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-23 09:54:06 +0900

    Fix list item z-index, unify PC hover buttons with task style (v26)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v25';
+const CACHE = 'task-v26';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
