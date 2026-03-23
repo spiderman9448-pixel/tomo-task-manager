b141ebc0d4d0aeddca0607d0d89fa3bbf9f10581
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-23 10:23:55 +0900

    Show cal memo preview below event name, tap to edit (v33)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v32';
+const CACHE = 'task-v33';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
