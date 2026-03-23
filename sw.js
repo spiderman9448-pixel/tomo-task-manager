72c59364b7e7787f8151408b93cb394c67d06eec
spiderman9448-pixel <spiderman9448@gmail.com>
2026-03-23 09:59:45 +0900

    Unify list swipe with task swipe classes (v27)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-v26';
+const CACHE = 'task-v27';
 const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
