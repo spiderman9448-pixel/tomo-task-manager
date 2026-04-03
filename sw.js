009dc4830e9d974495728e5efba07b10117515af
spiderman9448-pixel <spiderman9448@gmail.com>
2026-04-03 16:12:58 +0900

    Fix auth persistence + IME composing + sw v42

--- sw.js
+++ sw.js
@@ -14,11 +14,14 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
     fetch(e.request)
       .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
         return res;
       })
       .catch(() => caches.match(e.request))
