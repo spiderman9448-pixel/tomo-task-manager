710a86b1eb5869dc7de29088c8a76c1195bbe1e1
spiderman9448-pixel <spiderman9448@gmail.com>
2026-04-27 20:28:51 +0900

    Add swipe on empty area below tasks to change date (sw v43)

---
+++ Library/Mobile Documents/com~apple~CloudDocs/macスクショ/task-app/sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'task-v43';
+const ASSETS = ['./index.html', './icon.svg', './icon-192.png', './icon-512.png', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
